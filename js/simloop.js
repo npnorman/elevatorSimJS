2edb4f52c9e441f52d8d687401123f9f5728f08c
npnorman <npn.not@gmail.com>
2025-06-14 12:38:10 -0400

    Added docs, and basic setup for html/js

---
+++ js/simloop.js
@@ -0,0 +1,9 @@
+var output = document.getElementById("output");
+
+function addToLog(logItem) {
+    output.innerHTML += logItem + "<br>";
+}
+
+addToLog("Elevator Sim Starting");
+addToLog("StartUp: Elevators=1, Floors=10");
+
